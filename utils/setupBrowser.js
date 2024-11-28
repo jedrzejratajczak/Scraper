5561acbb9f91f2a0fa0b55baec836b6908da5dab
Jedrzej Ratajczak <nilphym@gmail.com>
2024-11-28 21:29:41 +0100

    Ensure browser closes

--- utils/setupBrowser.js
+++ utils/setupBrowser.js
@@ -1,39 +1,50 @@
 import puppeteer from "puppeteer";
 
 const setupBrowser = async (url) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--disable-extensions",
-      "--disable-gpu",
-      "--disable-software-rasterizer",
-      "--no-zygote",
-      "--disable-background-timers-throttling",
-      "--disable-renderer-backgrounding",
-      "--disable-backgrounding-occluded-windows",
-      "--disable-breakpad",
-      "--disable-sync",
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-features=site-per-process",
-    ],
-  });
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--disable-extensions",
+        "--disable-gpu",
+        "--disable-software-rasterizer",
+        "--no-zygote",
+        "--disable-background-timers-throttling",
+        "--disable-renderer-backgrounding",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-breakpad",
+        "--disable-sync",
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-features=site-per-process",
+      ],
+    });
 
-  const page = await browser.newPage();
+    const timer = setTimeout(async () => {
+      if (browser && browser.connected) await browser.close();
+    }, 300000);
+    browser.on("disconnected", () => clearTimeout(timer));
 
-  await page.setRequestInterception(true);
-  page.on("request", (req) => {
-    if (["image", "stylesheet", "font"].includes(req.resourceType())) {
-      req.abort();
-    } else {
-      req.continue();
-    }
-  });
+    const page = await browser.newPage();
 
-  await page.goto(url);
-  await page.setViewport({ width: 1600, height: 900 });
+    await page.setRequestInterception(true);
+    page.on("request", (req) => {
+      if (["image", "stylesheet", "font"].includes(req.resourceType())) {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
 
-  return { page, browser };
+    await page.goto(url);
+    await page.setViewport({ width: 1600, height: 900 });
+
+    return { page, browser };
+  } catch (error) {
+    if (browser) await browser.close();
+    throw error;
+  }
 };
 
 export default setupBrowser;
