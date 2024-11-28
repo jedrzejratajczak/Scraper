14fc6d08cd11c4aa2b7abf2e79dc21d815023052
Jedrzej Ratajczak <nilphym@gmail.com>
2024-11-28 21:51:12 +0100

    Add user agent

--- utils/setupBrowser.js
+++ utils/setupBrowser.js
@@ -28,6 +28,10 @@ const setupBrowser = async (url) => {
 
     const page = await browser.newPage();
 
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+    );
+
     await page.setRequestInterception(true);
     page.on("request", (req) => {
       if (["image", "stylesheet", "font"].includes(req.resourceType())) {
