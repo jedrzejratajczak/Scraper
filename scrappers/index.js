358f928f138c7f8d4e973e79347b83ba39f97399
Jedrzej Ratajczak <nilphym@gmail.com>
2024-11-28 23:09:12 +0100

    Remove time logger

--- scrappers/index.js
+++ scrappers/index.js
@@ -1,31 +1,24 @@
-import { addLog } from "../utils/index.js";
 import scrapE from "./scrapE.js";
 import scrapG from "./scrapG.js";
 import scrapVC from "./scrapVC.js";
 import scrapA from "./scrapA.js";
 
 const scrap = async (page, scrapper) => {
-  const startTime = performance.now();
   const { acceptCookies, fetchProducts, goToNextPage } = scrapper;
 
-  try {
-    await acceptCookies(page);
+  await acceptCookies(page);
 
-    const products = [];
-    let nextPageAvailable;
+  const products = [];
+  let nextPageAvailable;
 
-    do {
-      const pageProducts = await fetchProducts(page);
-      products.push(...pageProducts);
+  do {
+    const pageProducts = await fetchProducts(page);
+    products.push(...pageProducts);
 
-      nextPageAvailable = await goToNextPage(page);
-    } while (nextPageAvailable);
+    nextPageAvailable = await goToNextPage(page);
+  } while (nextPageAvailable);
 
-    return products;
-  } finally {
-    const endTime = performance.now();
-    addLog(`Time ${url}\n` + (endTime - startTime) / 1000);
-  }
+  return products;
 };
 
 export { scrapE, scrapG, scrapVC, scrapA };
