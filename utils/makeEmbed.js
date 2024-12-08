e1344078274bf9a3519160b3703e435915e3af7a
Jedrzej Ratajczak <nilphym@gmail.com>
2024-12-08 15:13:47 +0100

    Make embeds not crash

--- utils/makeEmbed.js
+++ utils/makeEmbed.js
@@ -1,19 +1,37 @@
 import { EmbedBuilder } from "discord.js";
 
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 const makeEmbed = ({
   src = "Brak",
   price = "Brak",
   title = "Brak",
   productId = "Brak",
   href = "Brak",
-}) =>
-  new EmbedBuilder()
+}) => {
+  const embed = new EmbedBuilder()
     .setColor(0x0099ff)
     .setTitle(title)
-    .setURL(href)
-    .setThumbnail(src)
     .setFooter({ text: `ID: ${productId}` })
     .setTimestamp()
-    .addFields({ name: "Cena", value: price }, { name: "Url", value: href });
+    .addFields({ name: "Cena", value: price });
+
+  if (isValidUrl(src)) {
+    embed.setThumbnail(src);
+  }
+
+  if (isValidUrl(href)) {
+    embed.setURL(href).addFields({ name: "Url", value: href });
+  }
+
+  return embed;
+};
 
 export default makeEmbed;
