f6bd4252c6c096171a91e1ebdc32c82648219c96
jedrzejratajczak <mrozelek@gmail.com>
2025-11-04 19:58:38 +0100

    Revert

--- app/products/H.js
+++ app/products/H.js
@@ -23,23 +23,7 @@ async function getProducts(url) {
     })
   });
 
-  const rawText = await response.text();
-
-  let data;
-  try {
-    data = JSON.parse(rawText);
-  } catch (error) {
-    const errorDetails = {
-      message: 'Failed to parse JSON response',
-      parseError: error.message,
-      httpStatus: `${response.status} ${response.statusText}`,
-      contentType: response.headers.get('content-type'),
-      contentLength: rawText.length,
-      responsePreview: rawText.substring(0, 500),
-      url: url
-    };
-    throw new Error(JSON.stringify(errorDetails, null, 2));
-  }
+  const data = await response.json();
 
   return data.articles
     .filter(article => article.articleNumber)
